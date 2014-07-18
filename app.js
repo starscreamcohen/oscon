e8cd3314d82b6d15798c645ca24652c7b83d6782
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-07-18 16:57:33 -0400

    adds an infinite loop for images

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 
-$(document).ready(function() {
-	imageChange();
+$(window).load(function() {
+	
+	var infiniteRotator = {
+		init: function() 
+		{
+			var initialFadeIn = 1000;
+
+			var itemInterval = 5000;
+
+			var fadeTime = 500;
+
+			var numberOfItems = $('.rotating-item').length;
+
+			var currentItem = 0;
+
+			$('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+
+			var infiniteLoop = setInterval(function() {
+				$('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+
+				if(currentItem === numberOfItems -1){
+					currentItem = 0;
+				}
+				else {
+					currentItem++;
+				}
+				$('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+			}, itemInterval);
+		}
+	};
+	infiniteRotator.init();
+
+	/*$(function(){
+    var images = ['.project-index', '.newsfeed', '.forum', '.org-profile'],
+         imgIx = 0;
+
+    (function nextImage(){
+        $(images[imgIx++] || images[imgIx = 0, imgIx++]).hide().delay(100).fadeIn(1000).delay(5000).fadeOut(500, nextImage);
+    })();
+	});*/ 
 });
 
-	function imageChange() {
-		$('.project-index').fadeOut();
-	}
